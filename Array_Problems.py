f427f4f7295bd2c304461daf13900132adb29a72
vikas <vikaszende2004@gmail.com>
2026-06-06 12:43:06 +0530

    Count Sub array problem

--- Array_Problems.py
+++ Array_Problems.py
@@ -1174,31 +1174,80 @@
 
 
 
-matrix = [
-    [1, 2, 3, 4],
-    [5, 6, 7, 8],
-    [9,10,11,12]
-]
-ans=[]
-n=len(matrix)
-top=0
-bottom=n-1
-left=0
-right=len(matrix[0])-1
-while top <= bottom and left <= right:
-    for i in range (left,right+1):
-        ans.append(matrix[top][i])
-    top+=1
-    for i in range (top,bottom+1):
-        ans.append(matrix[i][right])
-    right -= 1
-    if top <= bottom :
-        for i in range (right,left-1,-1):
-            ans.append(matrix[bottom][i])
-        bottom -=1
-    if left <= right:
-        for i in range(bottom, top - 1, -1):
-            ans.append(matrix[i][left])
-        left += 1
-print(ans)
+# matrix = [
+#     [1, 2, 3, 4],
+#     [5, 6, 7, 8],
+#     [9,10,11,12]
+# ]
+# ans=[]
+# n=len(matrix)
+# top=0
+# bottom=n-1
+# left=0
+# right=len(matrix[0])-1
+# while top <= bottom and left <= right:                                              # spiral matrix representation optimal meathod
+#     for i in range (left,right+1):
+#         ans.append(matrix[top][i])
+#     top+=1
+#     for i in range (top,bottom+1):
+#         ans.append(matrix[i][right])
+#     right -= 1
+#     if top <= bottom :
+#         for i in range (right,left-1,-1):
+#             ans.append(matrix[bottom][i])
+#         bottom -=1
+#     if left <= right:
+#         for i in range(bottom, top - 1, -1):
+#             ans.append(matrix[i][left])
+#         left += 1
+# print(ans)
     
+
+
+
+# arr=[1,2,3,-3,1,1,1,4,2,-3]
+# k=3
+# count=0
+# for i in range (len(arr)):
+#     for j in range (i,len(arr)):
+#         sum=0
+#         for k in range (i,j+1):
+#             sum=sum+arr[k]
+#         if sum==k:
+#             count+=1
+# print(count)
+
+
+
+
+
+
+# arr=[1,2,3,-3,1,1,1,4,2,-3]
+# k=3
+# count=0
+# for i in range (len(arr)):
+#     sum=0
+#     for j in range (i,len(arr)):
+#         sum+=arr[j]
+#         if sum==k:
+#            count+=1
+# print(count)
+
+
+
+
+arr=[1,2,3,-3,1,1,1,4,2,-3]
+k=3
+freq = {0: 1} 
+presum=0
+count=0
+for num in arr:
+    presum+=num
+    remove=presum-k
+    if remove in freq:
+        count += freq[remove]
+    freq[presum] = freq.get(presum, 0) + 1
+    
+
+
+print(count)
